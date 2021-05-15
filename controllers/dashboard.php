ed4306af8c41c8ee859041551dc8911ea4d31c3b
IIHGSII <zonahgs@gmail.com>
2021-05-15 19:07:09 -0400

    Controlador servicio

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -2,12 +2,35 @@
 class Dashboard extends SessionController{
     function __construct(){
         parent::__construct();
+        $this->user = $this->getUserSessionData();
         error_log('Dashboard::construct-> inicio de Dashboard');
     }
 
     function render(){
         error_log('Dashboard::render-> Carga el index de dashboard');
-        $this->view->render('dashboard/index');
+        $servicioModel = new ServicioModel();
+        $servicios = $this->getServicio(5);
+        $totalThisMonth = $servicioModel->getTotalAmountThisMonth($this->user->getId()); //Verificar si no debe ser cedula
+        $this->view->render('dashboard/index',[
+            'user' => $this->user,
+            'servicio' => $servicios,
+            'totalAmountThisMonth' => $totalThisMonth
+        ]);
+    }
+
+    private function getServicio($n = 0){
+        if($n < 0) return NULL;
+
+        $servicios = new ServicioModel();
+        return $servicioModel;
+    }
+
+    public function getActivo(){
+
+    }
+
+    public function getPersonal(){
+
     }
 
 }
